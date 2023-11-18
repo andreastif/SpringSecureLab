48f937049639d7da2688a26c55f28d3d9dfe3325
andreastif <45339645+andreastif@users.noreply.github.com>
2023-11-18 17:50:20 +0100

    Initial commit

---
+++ src/test/java/com/auth/authserver2/AuthServer2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.auth.authserver2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServer2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
